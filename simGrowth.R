3d91655e4f4ce40f3fee467f774d38813b7b61da
bletcher <bletcher@eco.umass.edu>
2016-12-09 16:15:07 -0500

    added yoy

--- simGrowth.R
+++ simGrowth.R
@@ -16,9 +16,8 @@ initSizeSD <- 4
 # residual growth error
 gSD <- 2
 
-growth <- array(NA,c(nSeasons,2))
-
-# 1st column is mean growth, second col is size-dep coeff
+# 1st column is mean growth, second col is size-dep coeff 
+# rows are seasons
 growth <- matrix( 
   c( 25, 5, 
      10, 0,
@@ -32,14 +31,20 @@ dat <- matrix(NA,nInd,nOcc)#, dimnames=list(1:nOcc, 1:T))
 meanLen <- matrix(NA,nOcc,nRivers+1)
 sdLen <- matrix(NA,nOcc,nRivers+1)
 
-for (i in 1:nInd){ dat[i,1] <- rnorm(1,initSize,initSizeSD) } # preload occ 1 
+dat[,1] <- rnorm(nInd,initSize,initSizeSD) # preload occ 1 
+
+# individual random slope multiplier on current body size
+# it is proportional to initial size
+ranSlope <- (dat[,1] - mean(dat[,1]))/sd(dat[,1]) * 0.75 
 
 for (t in 1:(nOcc-1)){
   seas <- (t - 2) %% nSeasons # (t-2) to offset season so occ1=seas3
   if(seas == 0) seas <- nSeasons
   for (i in 1:nInd){    
       dat[i,t+1] <- dat[i,t] + 
-                   ( growth[seas,1] + growth[seas,2]*(dat[i,t] - mean(dat[,t]))/sd(dat[,t]) ) +
+                   ( growth[seas,1] +                                            # intercept   
+                     growth[seas,2] * (dat[i,t] - mean(dat[,t]))/sd(dat[,t]) +   #size-dep growth
+                     ranSlope[i] *    (dat[i,t] - mean(dat[,t]))/sd(dat[,t]) ) + #individual random effect on growth
                     rnorm(1,0,gSD)
   }
   meanLen[t,1] <- mean(dat[,t])
@@ -55,12 +60,17 @@ names(d) <- c("id","l")
 d$occ <- rep(1:nOcc,nInd)
 d$seas <- rep( rep((c(3,4,1,2)), nOcc/nSeasons, length.out=nOcc), nInd ) #start with season 3
 d$riverDATA <- 1
+d$ranSlope <- rep(ranSlope,each=nOcc)
 
 d <- d %>% 
   group_by(id) %>%
   mutate( minOcc = min(occ), fOcc = (occ==minOcc)*1,
-          maxOcc = max(occ), lOcc = (occ==maxOcc)*1
+          maxOcc = max(occ), lOcc = (occ==maxOcc)*1,
+          lagLen = lead(l),
+          gr = lagLen - l
         )
+
+
 evalRows <- which( d$lOcc == 0 )
 firstObsRows <- which( d$fOcc == 1 )
 lastObsRows <- which( d$lOcc == 1 )
@@ -69,9 +79,12 @@ nEvalRows <- length(evalRows)
 nFirstObsRows <- length(firstObsRows)
 nLastObsRows <- length(lastObsRows)
 
-d$lNA <- ifelse(runif(nrow(d),0,1) < pNA & d$fOcc != 1, NA, d$l) #always have a r for fOcc
+d$lNA <- ifelse(runif(nrow(d),0,1) < pNA & d$fOcc != 1, NA, d$l) #always have a l for fOcc
 
- ggplot(d, aes(occ,l)) + geom_line(aes(color=id))
+ ggplot(d, aes(occ,lNA, group=id)) + geom_line(aes(color=ranSlope)) + scale_color_gradient2() #+ theme(legend.position="none")
+
+cutoffYOYData <- matrix(NA,nSeasons,nRivers+1) 
+cutoffYOYData[,1] <- 110 
 
 sink("gr.jags")
 cat("
@@ -93,15 +106,33 @@ cat("
         lengthSD[ season[ lastObsRows[i]],riverDATA[ lastObsRows[i] ] ]
     }
 
+    ############################
+    ###### YOY variable ########
+    ############################ 
+    # 1 for YOY, 2 for not YOY
+  
+    for( i in 1:nEvalRows ){
+      isYOY1[ evalRows[ i ] ] ~ dinterval( length[ evalRows[ i ] ], 
+                                           cutoffYOYDATA[ season[ evalRows[i] ],riverDATA[ evalRows[ i ] ] ] #, year[ evalRows[ i ] ] ] 
+      ) 
+       isYOY[ evalRows[ i ] ] <- isYOY1[ evalRows[ i ] ] + 1
+  
+    }
+  
+    # isYOY for last observation for each individual
+    for( i in 1:( nLastObsRows ) ){ 
+  
+      isYOY1[ lastObsRows[ i ] ] ~ dinterval( length[ lastObsRows[ i ] ], 
+                                           cutoffYOYDATA[ season[ lastObsRows[i] ],riverDATA[ lastObsRows[ i ] ] ] #, year[ lastObsRows[ i ] ] ] 
+      )
+      isYOY[ lastObsRows[ i ] ] <- isYOY1[ lastObsRows[ i ] ] + 1
+    } 
 
 
     ############################
     ###### growth model ########
     ############################
 
-#    for( i in 1:nFirstObsRows ){
-#      length[ firstObsRows[i] ] <- lengthDATA[ firstObsRows[i] ]
-#    }
     for( i in 1:nFirstObsRows ){
       length[ firstObsRows[i] ] ~ dnorm( 65,0.001 )   
       lengthDATA[ firstObsRows[i] ] ~ dnorm( length[ firstObsRows[i] ],9 ) # length measurement error
@@ -111,7 +142,7 @@ cat("
       
       length[ evalRows[i]+1 ] <- length[ evalRows[i] ] + gr[ evalRows[i] ] 
       
-      lengthDATA[ evalRows[i] + 1 ] ~ dnorm( length[ evalRows[i] + 1 ], 9/1 ) # length measurement error 
+      lengthDATA[ evalRows[i] + 1 ] ~ dnorm( length[ evalRows[i] + 1 ], 9 ) # length measurement error 
       
       gr[ evalRows[i] ] ~ dnorm( expectedGR[ evalRows[i] ], 
                                  1/expectedGRSigma[ evalRows[i] ]^2 
@@ -120,12 +151,13 @@ cat("
       expectedGR[ evalRows[i] ]  <- 
         grBetaInt[ season[ evalRows[i] ],riverDATA[ evalRows[i] ] ] 
       + grBeta[ 1, season[ evalRows[i] ],riverDATA[ evalRows[i] ] ] * stdLength[ evalRows[ i ] ]
+      + grBeta[ 2, season[ evalRows[i] ],riverDATA[ evalRows[i] ] ] * isYOY[ evalRows[ i ] ]
 
       expectedGRSigma[ evalRows[i] ] <- grSigmaBeta[ season[ evalRows[i] ],riverDATA[ evalRows[i] ] ]   
     }
 
   ############## Growth Priors ###############
-
+ 
     for( r in 1:( nRivers ) ){
 
 #      grBetaInt[ 1,r ] ~ dnorm( 8, 0.01 )
@@ -134,14 +166,20 @@ cat("
 #      grBetaInt[ 4,r ] ~ dnorm( 10, 0.01 )
 
       for( s in 1:4 ){   
-
+        
         grSigmaBeta[ s,r ] ~ dgamma(0.001,0.001)   
-        grBetaInt[ s,r ] ~ dnorm( 0,0.0001 )
-        grBeta[ 1,s,r ] ~ dnorm( 0,0.0001 )
+          grBetaInt[ s,r ] ~ dnorm( 0,0.0001 )
+         
+        for(i in 1:2){
+           grBeta[ i,s,r ] ~ dnorm( 0,0.0001 )
+        }
       }
     }
+
+
   
-  } # model
+  
+} # model
     ",fill = TRUE)
 sink()
 
@@ -153,11 +191,12 @@ data <- list( lengthDATA=d$lNA,
               nFirstObsRows = nFirstObsRows, firstObsRows = firstObsRows,
               nLastObsRows = nLastObsRows, lastObsRows = lastObsRows,
               nSeasons = nSeasons,
-              lengthMean = meanLen, lengthSD = sdLen
+              lengthMean = meanLen, lengthSD = sdLen,
+              cutoffYOYDATA = cutoffYOYData 
             )
 
 inits <- function(){
-  list(grBetaInt=array(rnorm(nSeasons*nRivers,0,2.25),c(nSeasons,nRivers)))
+  list(grBetaInt=array(rnorm(2*nSeasons*nRivers,0,2.25),c(2,nSeasons,nRivers)))
 }
 
 params <- c("grBetaInt","grBeta","grSigmaBeta")#, "riverDATA")
@@ -167,10 +206,10 @@ out <- jags(data = data,
             parameters.to.save = params,
             model.file = "gr.jags",
             n.chains = 3,
-            n.adapt = 100,
-            n.iter = 1000,
+            n.adapt = 1000, #1000
+            n.iter = 1000, # with pNA>0.25, need to run 50,000 iters, otherwise rhats are >1.1
             n.burnin = 500,
-            n.thin = 2,
+            n.thin = 4,
             parallel = TRUE
            )
 
